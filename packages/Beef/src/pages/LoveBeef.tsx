2b53f31d469fddaf5fd542954490638aa0751288
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2022-09-17 09:59:00 +0530

    init

---
+++ packages/Beef/src/pages/LoveBeef.tsx
@@ -0,0 +1,34 @@
+export const LoveBeefPage = () => {
+  return (
+    <div>
+      <h1>Love Beef</h1>
+      <p>
+        Bacon ipsum dolor amet shankle alcatra shoulder t-bone pork chop.
+        Andouille filet mignon tail tongue, ham hock jowl hamburger kielbasa.
+        Turducken ball tip chuck pork chop. Doner pancetta fatback bacon ground
+        round, drumstick pork flank meatball pig buffalo corned beef t-bone
+        andouille. Short ribs ball tip rump leberkas, bresaola kevin andouille.
+        Beef short ribs picanha chislic, porchetta pork belly brisket buffalo
+        bresaola prosciutto filet mignon flank cow turducken.
+      </p>
+      <p>
+        Kielbasa rump cow alcatra boudin turkey. Corned beef pastrami kevin
+        shank landjaeger. Ham hock shankle meatloaf filet mignon prosciutto
+        pork. Pastrami alcatra landjaeger, tail picanha sirloin turducken ham
+        hock flank short loin tri-tip t-bone. Jerky turkey shoulder salami
+        capicola burgdoggen prosciutto bacon tri-tip drumstick ball tip. Ham
+        corned beef meatloaf, jerky porchetta spare ribs prosciutto chuck
+        tri-tip andouille shoulder tail.
+      </p>
+      <p>
+        Tenderloin kielbasa burgdoggen porchetta, shankle shoulder picanha spare
+        ribs doner fatback corned beef ribeye jowl pancetta tri-tip. Jerky
+        porchetta fatback beef. Pastrami spare ribs pork chop, pork belly strip
+        steak drumstick bacon ground round. Pork loin bacon cow, frankfurter
+        chislic drumstick swine meatloaf tri-tip andouille. Tri-tip andouille
+        jerky, porchetta chuck short loin meatloaf flank swine shankle tail
+        jowl.
+      </p>
+    </div>
+  );
+};
